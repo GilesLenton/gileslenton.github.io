ab90fd2bc51d78e194c288741bb8abf3d1cbd3be
benjaminleonard <hello@benleonard.co.uk>
2016-11-08 16:58:19 +0000

    Removing lazy loading

--- js/load.js
+++ js/load.js
@@ -2,3 +2,21 @@ var panel = document.getElementById('container');
 var nav = document.getElementById('navigation');
 panel.classList.add('load');
 navigation.classList.add('load');
+
+// Fade images when loaded
+var images = document.getElementsByClassName('fade-img')
+
+Array.prototype.forEach.call(images, function(e) {
+  function loaded(image) {
+    console.log(image);
+    image.parentNode.classList.add('fadein');
+  }
+
+  console.log(e);
+
+  if (e.complete) {
+    loaded(e);
+  } else {
+    e.addEventListener('load', loaded(e));
+  }
+});
